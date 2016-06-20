4c612769455e6aa4573b6379892e67593e7613b2
CoderESalazar <eds_27@yahoo.com>
2016-06-19 17:57:19 -0700

    Try this out and see what happens.
    Test

---
+++ SectionDemo/SectionDemo/ViewModels/ListModelsVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SectionDemo.ViewModels
+{
+    public class ListModelsVM
+    {
+        public List<Properties> EmployeeSettings { get; set; }
+        public List<Properties> NameSettings { get; set; }
+    }
+    public class Properties
+    {
+        public int SectionNumber { get; set; }
+        public string SectionName { get; set; }
+        public string DictSectionName { get; set; }
+        public int? SortOrder { get; set; }
+        public string Description { get; set; }
+        public bool Display { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
